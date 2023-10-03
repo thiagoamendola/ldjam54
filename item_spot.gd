d4f2e5a05fbc6999d66faf1d8b74a0b7a95770fe
Thiago Amendola <amendolathiago@gmail.com>
2023-10-02 18:06:52 -0700

    Make item slot and dragging work for simple atom item

--- item_spot.gd
+++ item_spot.gd
@@ -1,20 +1,24 @@
-extends TextureRect
-
-var occupied := false
-
-#var DRAGGABLE_ITEM = load("res://draggable_item.tscn")
+extends Control
 
 var item
 
+func _ready():
+	item = $DraggableItem
+	pass # Replace with function body.
+
+
+func _process(delta):
+	pass
+
+# Could be a generic draggable class
 func _get_drag_data(position):
 	if item == null:
 		return null
 
 	var drag_item = item
-	var control = Control.new()
 	remove_child(drag_item)
+	var control = Control.new()
 	control.add_child(drag_item)
-	drag_item.set_position(-position)
 	set_drag_preview(control)
 	item = null
 	return drag_item
@@ -27,4 +31,5 @@ func _drop_data(at_position, data):
 	item.get_parent().remove_child(item)
 	item.set_anchors_and_offsets_preset(Control.PRESET_CENTER, Control.PRESET_MODE_KEEP_SIZE)
 	add_child(item)
+
 
